05e2ed91cdb23d2b4bed6c7b1fa4f2eee9b88de8
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-23 20:28:33 -0300

    project: create & setup node-config default file

---
+++ config/default.ts
@@ -0,0 +1,27 @@
+export interface PostgresConfigProps {
+  host: string;
+  port: number;
+  username: string;
+  password: string;
+  database: string;
+}
+
+export interface ConfigProps {
+  app: {
+    port: number;
+    database: PostgresConfigProps;
+  }
+}
+
+export default {
+  app: {
+    port: 3000,
+    database: {
+      host: process.env.PG_HOST,
+      port: 5432,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASS,
+      database: process.env.PG_NAME
+    }
+  }
+} as ConfigProps;
