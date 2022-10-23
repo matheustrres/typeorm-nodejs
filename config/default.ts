b5c943fbbbec8e03a4d1d4180190c2555240618e
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-23 20:34:22 -0300

    refactor: add PG_PORT env variable

--- config/default.ts
+++ config/default.ts
@@ -18,7 +18,7 @@ export default {
     port: 3000,
     database: {
       host: process.env.PG_HOST,
-      port: 5432,
+      port: process.env.PG_PORT || 5432,
       username: process.env.PG_USER,
       password: process.env.PG_PASS,
       database: process.env.PG_NAME
