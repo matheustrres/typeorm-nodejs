0403fac382125bafd9beee922a333fa27a37dabb
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-23 20:32:07 -0300

    feat: bootstrap server after successfully initializing AppDataSource

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
-import { Server } from './shared/infra/http/server';
+import 'reflect-metadata';
+
+import { Server } from '@/src/shared/infra/http/server';
+import { AppDataSource } from '@/src/shared/infra/typeorm/data-source';
 
 (async (): Promise<void> => {
-  const server: Server = new Server();
+  AppDataSource.initialize()
+    .then(
+      (): void => {
+        const server: Server = new Server();
 
-  server.initialize();
-  server.startServer();
+        server.initialize();
+        server.startServer();
+      }
+    );
 })();
