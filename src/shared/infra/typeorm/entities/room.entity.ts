34e2a04ce98bb170c58cccf55ec8e965763de7c6
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-24 10:53:01 -0300

    feat: establish relation with subject entity

--- src/shared/infra/typeorm/entities/room.entity.ts
+++ src/shared/infra/typeorm/entities/room.entity.ts
@@ -2,9 +2,11 @@ import { randomUUID } from 'node:crypto';
 import {
   Column,
   Entity,
+  ManyToMany,
   OneToMany,
   PrimaryGeneratedColumn
 } from 'typeorm';
+import { SubjectEntity } from './subject.entity';
 
 import { VideoEntity } from './video.entity';
 
@@ -24,6 +26,12 @@ export class RoomEntity {
     (video: VideoEntity): RoomEntity => video.room
   )
   videos: VideoEntity[];
+
+  @ManyToMany((): typeof SubjectEntity => 
+    SubjectEntity,
+    (subject: SubjectEntity): RoomEntity[] => subject.rooms
+  )
+  subjects: SubjectEntity[];
 
   constructor() {
     if (!this.id) this.id = randomUUID();
