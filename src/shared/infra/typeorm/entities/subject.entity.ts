661814c058cd4832bc789cdc2c7a2b3083a418ef
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-24 10:52:45 -0300

    feat(typeorm/entities): create subject entity

---
+++ src/shared/infra/typeorm/entities/subject.entity.ts
@@ -0,0 +1,43 @@
+import { randomUUID } from 'node:crypto';
+import { 
+  Column, 
+  Entity, 
+  JoinTable, 
+  JoinTableOptions, 
+  ManyToMany, 
+  PrimaryGeneratedColumn 
+} from 'typeorm';
+
+import { RoomEntity } from './room.entity';
+
+const joinTableOptions: JoinTableOptions = {
+  name: 'room_subject',
+  joinColumn: {
+    name: 'room_id',
+    referencedColumnName: 'id'
+  },
+  inverseJoinColumn: {
+    name: 'subject_id',
+    referencedColumnName: 'id'
+  }
+}
+
+@Entity('subjects')
+export class SubjectEntity {
+  @PrimaryGeneratedColumn()
+  id: string;
+
+  @Column({ type: 'text' })
+  name: string;
+
+  @ManyToMany((): typeof RoomEntity => 
+    RoomEntity,
+    (room: RoomEntity): SubjectEntity[] => room.subjects
+  )
+  @JoinTable(joinTableOptions)
+  rooms: RoomEntity[];
+
+  constructor() {
+    if (!this.id) this.id = randomUUID();
+  }
+}
