8f5f742ed035c3e939f17bd5184c2d61bdc82c06
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-25 12:23:16 -0300

    feat: delete subject repository interface and its implementation

--- src/core/domain/repositories/subject.repository.ts
+++
@@ -1,7 +0,0 @@
-import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
-
-export interface ISubjectRepository {
-  create(name: string): Promise<SubjectEntity>;
-  findById(id: string): Promise<SubjectEntity | undefined>
-  findByName(name: string): Promise<SubjectEntity | undefined>;
-}
