2a5e18d83b2c8286d5e275982a7102dc08d6e8c6
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-25 11:13:09 -0300

    feat(http/controllers): create SubjectController & extend BaseController

---
+++ src/shared/infra/http/controllers/subject.controller.ts
@@ -0,0 +1,36 @@
+import { Request, Response } from 'express';
+import { Controller, Get, Post } from '@overnightjs/core';
+
+import { BaseController } from './base.controller';
+
+import { SubjectEntity } from '../../typeorm/entities/subject.entity';
+import { SubjectService } from '@/src/services/subject.service';
+
+@Controller('subject')
+export class SubjectController extends BaseController {
+  constructor(private service: SubjectService = new SubjectService()) {
+    super();
+  }
+
+  @Post('')
+  public async create(request: Request, response: Response): Promise<Response> {
+    try {
+      const subject: SubjectEntity = await this.service.create(request.body.name);
+
+      return response.status(201).send(subject);
+    } catch (error) {
+      return this.sendErrorResponse(response, error);
+    }
+  }
+
+  @Get(':id')
+  public async get(request: Request, response: Response): Promise<Response> {
+    try {
+      const subject: SubjectEntity = await this.service.findById(request.params.id);
+
+      return response.status(200).send(subject);
+    } catch (error) {
+      return this.sendErrorResponse(response, error);
+    }
+  }
+}
