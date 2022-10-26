b947ef7b8113617a8d6da1ce9c92c19d76e67281
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-26 15:24:08 -0300

    feat: turn repository into private

--- src/services/subject.service.ts
+++ src/services/subject.service.ts
@@ -4,9 +4,10 @@ import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entit
 import { DatabaseValidationError } from '@/src/shared/utils/errors/database.error';
 
 import { ORMSubjectRepository } from '@/src/core/infra/repositories/implementations/subject.repository';
+import { SubjectRepository } from '@/src/core/domain/repositories/typeorm/subject/subject.repository';
 
 export class SubjectService {
-  constructor(protected repository = new ORMSubjectRepository()) {}
+  constructor(private repository: SubjectRepository = new ORMSubjectRepository()) {}
 
   public async create(data: SubjectDto): Promise<SubjectEntity> {
     return this.repository.create(data);
