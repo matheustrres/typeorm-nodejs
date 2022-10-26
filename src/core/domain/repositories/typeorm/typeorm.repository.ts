95c9d91c68f62bab963eb1eaeff039c27d0c9f41
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-26 11:53:09 -0300

    chore: setup entityRepository getting repository from AppDataSource
    - change internal methods methodology by using entityRepository

--- src/core/domain/repositories/typeorm/typeorm.repository.ts
+++ src/core/domain/repositories/typeorm/typeorm.repository.ts
@@ -1,51 +1,56 @@
-import { BaseEntity } from 'typeorm';
+import { 
+  EntityTarget, 
+  Repository, 
+  FindOneOptions,
+  BaseEntity
+} from 'typeorm';
 
-import { FilterOptions } from '../base.repository';
 import { MainRepository } from '../main.repository';
 
-import { 
-  DatabaseError, 
-  DatabaseInternalError, 
-  DatabaseValidationError 
+import {
+  DatabaseError,
+  DatabaseInternalError,
+  DatabaseValidationError
 } from '@/src/shared/utils/errors/database.error';
 
 import { Logger } from '@/src/shared/utils/logger';
+
+import { AppDataSource } from '@/src/shared/infra/typeorm/data-source';
 
 /**
  * A TypeORM repository abstract class for repository classes to extend
  * 
  * @abstract
- * @template {Object} T - The DTO for its entity  
  * @template {Object} E - A TypeORM entity that must extend BaseEntity
+ * @template {Object} T - A Dto which extends E 
  */
-export abstract class TypeORMRepository<T, E extends BaseEntity> extends MainRepository<T, E> {
+export abstract class TypeORMRepository<E extends BaseEntity, T extends E> extends MainRepository<E, T> {
   private logger: Logger;
+  private entityRepository: Repository<E>;
 
-  constructor(private entity: E) {
+  constructor(private entityTarget: EntityTarget<E>) {
     super();
 
+    this.entityRepository = AppDataSource.getRepository(this.entityTarget);
     this.logger = Logger.it(this.constructor.name);
   }
 
   public async create(data: T): Promise<E> {
     try {
-      const record: E = await this.entity.save({ data });
-
+      const record: E = this.entityRepository.create(data);
+      await this.entityRepository.save(record);
+      
       return record;
     } catch (error) {
       this.handleError(error);
     }
   }
 
-  public async find(options: FilterOptions): Promise<E> {
+  public async find(options: FindOneOptions<E>): Promise<E> {
     try {
-      const record: E = await this.entity.recover({
-        data: {
-          options
-        }
-      });
+      const record: E = await this.entityRepository.findOne(options);
 
-      return record;
+      return record
     } catch (error) {
       this.handleError(error)
     }
