eee65e9fa16e62bba733132ca98854b0762d753a
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-26 11:56:50 -0300

    fix: change generic allocation order & make search with where clause

--- src/core/domain/repositories/typeorm/subject/subject.repository.ts
+++ src/core/domain/repositories/typeorm/subject/subject.repository.ts
@@ -1,9 +1,9 @@
-import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 import { SubjectDto } from '@/src/core/domain/dtos/subject.dto';
+import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
 import { BaseRepository } from '@/src/core/domain/repositories/base.repository';
 
-export interface SubjectRepository extends BaseRepository<SubjectDto, SubjectEntity> {
+export interface SubjectRepository extends BaseRepository<SubjectEntity, SubjectDto> {
   findById(id: string): Promise<SubjectEntity | undefined>;
   findByName(name: string): Promise<SubjectEntity | undefined>;
 }
--- src/core/infra/repositories/implementations/subject.repository.ts
+++ src/core/infra/repositories/implementations/subject.repository.ts
@@ -1,20 +1,29 @@
+import { EntityTarget } from 'typeorm';
+
 import { SubjectDto } from '@/src/core/domain/dtos/subject.dto';
-
 import { SubjectRepository } from '@/src/core/domain/repositories/typeorm/subject/subject.repository';
-import { TypeORMRepository } from '@/src/core/domain/repositories/typeorm/typeorm.repository';
-
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
-export class ORMSubjectRepository extends TypeORMRepository<SubjectDto, SubjectEntity> implements SubjectRepository {
-  constructor(entity: SubjectEntity = new SubjectEntity()) {
+import { TypeORMRepository } from '@/src/core/domain/repositories/typeorm/typeorm.repository';
+
+export class ORMSubjectRepository extends TypeORMRepository<SubjectEntity, SubjectDto> implements SubjectRepository {
+  constructor(private entity: EntityTarget<SubjectEntity> = SubjectEntity) {
     super(entity);
   }
 
   public async findById(id: string): Promise<SubjectEntity> {
-    return this.find({ id });
+    return this.find({
+      where: {
+        id
+      }
+    });
   }
 
   public async findByName(name: string): Promise<SubjectEntity> {
-    return this.find({ name });
+    return this.find({
+      where: {
+        name
+      }
+    });
   }
 }
