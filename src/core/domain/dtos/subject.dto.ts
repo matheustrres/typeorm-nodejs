7c5cb0e02958a9f0768da88437550eee6937b63b
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-27 10:10:48 -0300

    feat: use class-validator over CreateSubjectDto props

--- src/core/domain/dtos/subject.dto.ts
+++ src/core/domain/dtos/subject.dto.ts
@@ -1,3 +1,15 @@
+import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
+
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
+import { RoomEntity } from '@/src/shared/infra/typeorm/entities/room.entity';
 
 export interface SubjectDto extends SubjectEntity {}
+
+export class CreateSubjectDto implements Pick<SubjectDto, 'name' | 'rooms'> {
+  @IsString()
+  @IsNotEmpty()
+  name: string;
+  
+  @IsOptional()
+  rooms: RoomEntity[];
+}
