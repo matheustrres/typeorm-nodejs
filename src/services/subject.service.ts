9a24ab0b2ca6e328af128341a4f534ca93be3475
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-28 15:12:49 -0300

    refactor: change variable names and do verifications

--- src/services/subject.service.ts
+++ src/services/subject.service.ts
@@ -1,4 +1,5 @@
 import { CreateSubjectDto } from '@/src/core/domain/dtos/subject.dto';
+
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
 import { DatabaseValidationError } from '@/src/shared/utils/errors/database.error';
@@ -10,29 +11,27 @@ export class SubjectService {
   constructor(private repository: SubjectRepository = new ORMSubjectRepository()) {}
 
   public async create(data: CreateSubjectDto): Promise<SubjectEntity> {
+    const subjectAlreadyExists: SubjectEntity = await this.repository.findByName(data.name);
+    
+    if (subjectAlreadyExists) {
+      throw new DatabaseValidationError(
+        'Subject already exists with the given name',
+        'DUPLICATED'
+      );
+    }
+    
     return this.repository.create(data);
   }
-
-  public async findByName(name: string): Promise<SubjectEntity> {
-    const subject: SubjectEntity = await this.repository.findByName(name);
-
-    if (!subject)
-      throw new DatabaseValidationError(
-        'No subject were found',
-        'INVALID'
-      );
-
-    return subject;
-  }
-
+  
   public async findById(id: string): Promise<SubjectEntity> {
     const subject: SubjectEntity = await this.repository.findById(id);
 
-    if (!subject)
+    if (!subject) {
       throw new DatabaseValidationError(
         'No subject were found',
         'INVALID'
       );
+    }
 
     return subject;
   }
