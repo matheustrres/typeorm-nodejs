b9f4495ed9332b6fa1c9e0d96ddea993667a6424
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-27 23:56:17 -0300

    feat: create ORMStudentRepository

---
+++ src/core/infra/repositories/implementations/student.repository.ts
@@ -0,0 +1,12 @@
+import { EntityTarget } from 'typeorm';
+
+import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
+
+import { TypeORMRepository } from '@/src/core/domain/repositories/typeorm/typeorm.repository';
+import { StudentRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
+
+export class ORMStudentRepository extends TypeORMRepository<StudentEntity> implements StudentRepository {
+  constructor(entity: EntityTarget<StudentEntity> = StudentEntity) {
+    super(entity);
+  }
+}
