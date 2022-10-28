36da7bbb2e0a6e8ea06737012fdaf0f05c709fcc
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-28 19:08:48 -0300

    feat(services): create StudentService

---
+++ src/services/student.service.ts
@@ -0,0 +1,38 @@
+import { CreateStudentDto } from '@/src/core/domain/dtos/student.dto';
+
+import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
+
+import { DatabaseValidationError } from '@/src/shared/utils/errors/database.error';
+
+import { ORMStudentRepository } from '@/src/core/infra/repositories/implementations/student.repository';
+import { StudentRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
+
+export class StudentService {
+  constructor(private repository: StudentRepository = new ORMStudentRepository()) {}
+  
+  public async create(data: CreateStudentDto): Promise<StudentEntity> {
+    const studentAlreadyExists: StudentEntity = await this.repository.findByEmail(data.email);
+    
+    if (studentAlreadyExists) {
+      throw new DatabaseValidationError(
+        'Email already taken',
+        'DUPLICATED'
+      );
+    }
+    
+    return this.repository.create(data);
+  }
+  
+  public async findById(id: string): Promise<StudentEntity> {
+    const student: StudentEntity = await this.repository.findById(id);
+    
+    if (!student) {
+      throw new DatabaseValidationError(
+        'No student were found',
+        'INVALID'
+      );
+    }
+    
+    return student;
+  }
+}
