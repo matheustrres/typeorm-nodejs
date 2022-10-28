0309be1ef5430641e22720f8666e111ca176be99
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-27 23:56:30 -0300

    feat: create ORMRoomRepository

---
+++ src/core/infra/repositories/implementations/room.repository.ts
@@ -0,0 +1,12 @@
+import { EntityTarget } from 'typeorm';
+
+import { RoomEntity } from '@/src/shared/infra/typeorm/entities/room.entity';
+import { RoomRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
+
+import { TypeORMRepository } from '@/src/core/domain/repositories/typeorm/typeorm.repository';
+
+export class ORMRoomRepository extends TypeORMRepository<RoomEntity> implements RoomRepository {
+  constructor(entity: EntityTarget<RoomEntity> = RoomEntity) {
+    super(entity);
+  }
+}
