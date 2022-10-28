74be397849770ae9f0cec02ffbb7a6002127ca64
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-27 23:55:12 -0300

    fix: import path

--- src/core/infra/repositories/implementations/subject.repository.ts
+++ src/core/infra/repositories/implementations/subject.repository.ts
@@ -1,9 +1,9 @@
 import { EntityTarget } from 'typeorm';
 
-import { SubjectRepository } from '@/src/core/domain/repositories/typeorm/subject/subject.repository';
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
 import { TypeORMRepository } from '@/src/core/domain/repositories/typeorm/typeorm.repository';
+import { SubjectRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
 
 export class ORMSubjectRepository extends TypeORMRepository<SubjectEntity> implements SubjectRepository {
   constructor(entity: EntityTarget<SubjectEntity> = SubjectEntity) {
