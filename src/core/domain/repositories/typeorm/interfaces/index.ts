8e09797a36a6f9171f42a1afe6ff67b8835384c5
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-28 14:47:06 -0300

    feat: add findByName method to RoomRepository
    - add findByEmail method to StudentRepository

--- src/core/domain/repositories/typeorm/interfaces/index.ts
+++ src/core/domain/repositories/typeorm/interfaces/index.ts
@@ -4,9 +4,13 @@ import { RoomEntity } from '@/src/shared/infra/typeorm/entities/room.entity';
 import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
-export interface RoomRepository extends BaseRepository<RoomEntity> {}
+export interface RoomRepository extends BaseRepository<RoomEntity> {
+  findByName(name: string): Promise<RoomEntity | undefined>;
+}
 
-export interface StudentRepository extends BaseRepository<StudentEntity> {}
+export interface StudentRepository extends BaseRepository<StudentEntity> {
+  findByEmail(email: string): Promise<StudentEntity | undefined>;
+}
 
 export interface SubjectRepository extends BaseRepository<SubjectEntity> {
   findByName(name: string): Promise<SubjectEntity | undefined>;
