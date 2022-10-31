3af1c1c532b00b63e2a4645af8ad57118bb8db1b
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-31 16:03:53 -0300

    feat: remove methods that are now Profile related

--- src/services/student.service.ts
+++ src/services/student.service.ts
@@ -1,5 +1,3 @@
-import { CreateStudentDto } from '@/src/core/domain/dtos/student.dto';
-
 import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
 
 import { DatabaseValidationError } from '@/src/shared/utils/errors/database.error';
@@ -7,47 +5,8 @@ import { DatabaseValidationError } from '@/src/shared/utils/errors/database.erro
 import { ORMStudentRepository } from '@/src/core/infra/repositories/implementations/student.repository';
 import { StudentRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
 
-import { AuthProvider } from '@/src/shared/container/providers/auth.provider';
-
 export class StudentService {
   constructor(private repository: StudentRepository = new ORMStudentRepository()) {}
-  
-  public async authenticate(email: string, password: string) {
-    const student: StudentEntity = await this.repository.findByEmail(email);
-    
-    if (!student) {
-      throw new DatabaseValidationError(
-        'Invalid credentials',
-        'INVALID'
-      );
-    }
-    
-    const validPassword: boolean = await AuthProvider.comparePasswords(password, student.password);
-    
-    if (!validPassword) {
-      throw new DatabaseValidationError(
-        'Invalid credentials',
-        'INVALID'
-      );
-    }
-
-    return AuthProvider.signToken(student.id);
-  }
-  
-  public async create(data: CreateStudentDto): Promise<StudentEntity> {
-    const studentAlreadyExists: StudentEntity = await this.repository.findByEmail(data.email);
-    
-    if (studentAlreadyExists) {
-      throw new DatabaseValidationError(
-        'Email already taken',
-        'DUPLICATED'
-      );
-    }
-    
-    data.password = await AuthProvider.hashPassword(data.password);
-    
-    return this.repository.create(data);
-  }
   
   public async findById(id: string): Promise<StudentEntity> {
     const student: StudentEntity = await this.repository.findById(id);
--- src/shared/infra/http/controllers/student.controller.ts
+++ src/shared/infra/http/controllers/student.controller.ts
@@ -3,15 +3,12 @@ import {
   Controller,
   Get,
   Middleware,
-  Post
 } from '@overnightjs/core';
 
 import { BaseController } from './base.controller';
 
 import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
 import { StudentService } from '@/src/services/student.service';
-
-import { CreateStudentDto } from '@/src/core/domain/dtos/student.dto';
 
 import { AuthMiddleware } from '@/src/shared/infra/http/middlewares/auth.middleware';
 
@@ -28,32 +25,6 @@ export class StudentController extends BaseController {
       const me: StudentEntity = await this.service.findById(request.userId);
       
       return response.status(200).send(me);
-    } catch (error) {
-      return this.sendErrorResponse(response, error);
-    }
-  }
-  
-  @Post('')
-  public async create(request: Request, response: Response): Promise<Response> {
-    try {
-      const body: CreateStudentDto = request.body;
-      const student: StudentEntity = await this.service.create(body);
-      
-      return response.status(201).send(student);
-    } catch (error) {
-      return this.sendErrorResponse(response, error);
-    }
-  }
-  
-  @Post('auth')
-  public async authenticate(request: Request, response: Response): Promise<Response> {
-    try {
-      const email: string = request.body.email;
-      const password: string = request.body.password;
-      
-      const token = await this.service.authenticate(email, password);
-      
-      return response.status(200).send({ token });
     } catch (error) {
       return this.sendErrorResponse(response, error);
     }
