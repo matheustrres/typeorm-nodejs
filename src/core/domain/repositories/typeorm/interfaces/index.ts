b48a59b5b8a40f739b895a2de034c0cd1a2e68ba
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-31 15:58:06 -0300

    chore: create ProfileRepository & remove unused props from other repos

--- src/core/domain/repositories/typeorm/interfaces/index.ts
+++ src/core/domain/repositories/typeorm/interfaces/index.ts
@@ -1,8 +1,14 @@
 import { BaseRepository } from '@/src/core/domain/repositories/base.repository';
 
+import { ProfileEntity } from '@/src/shared/infra/typeorm/entities/profile.entity';
 import { RoomEntity } from '@/src/shared/infra/typeorm/entities/room.entity';
 import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
+
+export interface ProfileRepository extends BaseRepository<ProfileEntity> {
+  findById(id: string): Promise<ProfileEntity | undefined>;
+  findByEmail(email: string): Promise<ProfileEntity | undefined>;
+}
 
 export interface RoomRepository extends BaseRepository<RoomEntity> {
   findByName(name: string): Promise<RoomEntity | undefined>;
@@ -10,10 +16,8 @@ export interface RoomRepository extends BaseRepository<RoomEntity> {
 
 export interface StudentRepository extends BaseRepository<StudentEntity> {
   findById(id: string): Promise<StudentEntity | undefined>;
-  findByEmail(email: string): Promise<StudentEntity | undefined>;
 }
 
 export interface SubjectRepository extends BaseRepository<SubjectEntity> {
   findById(id: string): Promise<SubjectEntity | undefined>;
-  findByName(name: string): Promise<SubjectEntity | undefined>;
 }
