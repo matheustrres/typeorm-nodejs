039a5ad4abc44d8bd2bceccc897e253b56c6a84c
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-31 16:02:11 -0300

    feat(repos/implementations): create ORMProfileRepository properly

---
+++ src/core/infra/repositories/implementations/profile.repository.ts
@@ -0,0 +1,28 @@
+import { EntityTarget } from 'typeorm';
+
+import { ProfileEntity } from '@/src/shared/infra/typeorm/entities/profile.entity';
+import { ProfileRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
+
+import { TypeORMRepository } from '@/src/core/domain/repositories/typeorm/typeorm.repository';
+
+export class ORMProfileRepository extends TypeORMRepository<ProfileEntity> implements ProfileRepository {
+  constructor(entity: EntityTarget<ProfileEntity> = ProfileEntity) {
+    super(entity);
+  }
+  
+  public async findByEmail(email: string): Promise<ProfileEntity | undefined> {
+    return this.find({
+      where: {
+        email
+      }
+    });
+  }
+  
+  public async findById(id: string): Promise<ProfileEntity | undefined> {
+      return this.find({
+      where: {
+        id
+      }
+    });
+  }
+}
