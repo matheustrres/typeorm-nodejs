4692994515b3318ea181d211f48ee923e1f7f14c
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-31 16:06:11 -0300

    feat(services): create ProfileService

---
+++ src/services/profile.service.ts
@@ -0,0 +1,64 @@
+import { CreateProfileDto } from '@/src/core/domain/dtos/profile.dto';
+
+import { ProfileEntity } from '@/src/shared/infra/typeorm/entities/profile.entity';
+
+import { DatabaseValidationError } from '@/src/shared/utils/errors/database.error';
+
+import { ORMProfileRepository } from '@/src/core/infra/repositories/implementations/profile.repository';
+import { ProfileRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
+
+import { AuthProvider } from '@/src/shared/container/providers/auth.provider';
+
+export class ProfileService {
+  constructor(private repository: ProfileRepository = new ORMProfileRepository()) {}
+  
+  public async authenticate(email: string, password: string): Promise<string> {
+    const profile: ProfileEntity = await this.repository.findByEmail(email);
+    
+    if (!profile) {
+      throw new DatabaseValidationError(
+        'Invalid credentials',
+        'INVALID'
+      );
+    }
+    
+    const validPassword: boolean = await AuthProvider.comparePasswords(password, profile.password);
+    
+    if (!validPassword) {
+      throw new DatabaseValidationError(
+        'Invalid credentials',
+        'INVALID'
+      );
+    }
+    
+    return AuthProvider.signToken(profile.id);
+  }
+  
+  public async create(data: CreateProfileDto): Promise<ProfileEntity> {
+    const profileAlreadyExists: ProfileEntity = await this.repository.findByEmail(data.email);
+    
+    if (profileAlreadyExists) {
+      throw new DatabaseValidationError(
+        'Profile already exists',
+        'DUPLICATED'
+      );
+    }
+    
+    data.password = await AuthProvider.hashPassword(data.password);
+    
+    return this.repository.create(data);
+  }
+  
+  public async findById(id: string): Promise<ProfileEntity> {
+    const profile: ProfileEntity = await this.repository.findById(id);
+    
+    if (!profile) {
+      throw new DatabaseValidationError(
+        'No profile were found',
+        'INVALID'
+      );
+    }
+    
+    return profile;
+  }
+}
