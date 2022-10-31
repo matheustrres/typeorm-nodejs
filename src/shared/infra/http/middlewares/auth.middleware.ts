0f4d92993f0d7923beb21c930d1f117475a972ad
Matheus Torres <torresmiranda.rj@gmail.com>
2022-10-31 18:23:00 -0300

    fix: check for an existing header token

--- src/shared/infra/http/middlewares/auth.middleware.ts
+++ src/shared/infra/http/middlewares/auth.middleware.ts
@@ -2,8 +2,18 @@ import { NextFunction, Request, Response } from 'express';
 
 import { AuthProvider } from '@/src/shared/container/providers/auth.provider';
 
-export const AuthMiddleware = (request: Partial<Request>, response: Partial<Response>, next: NextFunction): void => {
+export const AuthMiddleware = (request: Partial<Request>, response: Partial<Response>, next: NextFunction): Response|void => {
   const tokenInHeader: string = request.headers.authorization;
+
+  if (!tokenInHeader) {
+    return response
+      .status?.(401)
+      .send({
+        code: 401,
+        error: 'Invalid token'
+      });
+  }
+  
   const [, token] = tokenInHeader.split(' ');
   
   try {
