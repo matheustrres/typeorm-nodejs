c25cc63de89bff94494aeb930c6a1c0f266c120d
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-01 15:20:53 -0300

    feat: remove 1-1 unidirectional relation with profile

--- src/shared/infra/typeorm/entities/student.entity.ts
+++ src/shared/infra/typeorm/entities/student.entity.ts
@@ -3,23 +3,13 @@ import { randomUUID } from 'node:crypto';
 import {
   CreateDateColumn,
   Entity,
-  JoinColumn,
-  JoinColumnOptions,
   ManyToMany,
   ObjectLiteral,
-  OneToOne,
   PrimaryColumn,
   UpdateDateColumn
 } from 'typeorm';
 
-import { ProfileEntity } from './profile.entity';
 import { SubjectEntity } from './subject.entity';
-
-const studentProfileJoinColumn: JoinColumnOptions = {
-  name: 'student_profile',
-  referencedColumnName: 'id',
-  foreignKeyConstraintName: 'fk_student_profile'
-}
 
 @Entity('students')
 export class StudentEntity implements ObjectLiteral {
@@ -28,15 +18,6 @@ export class StudentEntity implements ObjectLiteral {
   
   @ManyToMany(() => SubjectEntity, (subject) => subject.enrolledStudents)
   subjects: SubjectEntity[];
-  
-  @OneToOne(() =>
-    ProfileEntity,
-    {
-    cascade: true,
-    onDelete: 'CASCADE'
-  })
-  @JoinColumn(studentProfileJoinColumn)
-  profile: ProfileEntity;
   
   @CreateDateColumn()
   createdAt?: Date;
