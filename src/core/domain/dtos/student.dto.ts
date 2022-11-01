0ce234cb820ed58783d2649692f19d8dc0d5d629
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-01 15:24:05 -0300

    fix: remove property profile no longer owned

--- src/core/domain/dtos/student.dto.ts
+++ src/core/domain/dtos/student.dto.ts
@@ -1,15 +1,11 @@
 import { IsOptional } from 'class-validator';
 
-import { ProfileEntity } from '@/src/shared/infra/typeorm/entities/profile.entity';
 import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
 export interface StudentDto extends StudentEntity {}
 
-export class CreateStudentDto implements Pick<StudentDto, 'subjects' | 'profile'> {
+export class CreateStudentDto implements Pick<StudentDto, 'subjects'> {
   @IsOptional()
   subjects: SubjectEntity[];
-  
-  @IsOptional()
-  profile: ProfileEntity
 }
