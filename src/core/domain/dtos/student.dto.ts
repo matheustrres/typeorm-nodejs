990df64edbb2c2aa83181b0eae011f6e03dbceef
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-04 12:25:40 -0300

    fix: pick profile

--- src/core/domain/dtos/student.dto.ts
+++ src/core/domain/dtos/student.dto.ts
@@ -1,11 +1,15 @@
 import { IsOptional } from 'class-validator';
 
+import { ProfileEntity } from '@/src/shared/infra/typeorm/entities/profile.entity';
 import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
 export interface StudentDto extends StudentEntity {}
 
-export class CreateStudentDto implements Pick<StudentDto, 'subjects'> {
+export class CreateStudentDto implements Pick<StudentDto, 'subjects' | 'profile'> {
+  @IsOptional()
+  profile?: ProfileEntity;
+  
   @IsOptional()
   subjects: SubjectEntity[];
 }
