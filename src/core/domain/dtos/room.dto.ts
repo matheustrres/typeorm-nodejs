658e80b9773cb0e28247e42e95651b14e30a257b
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-04 12:25:25 -0300

    fix: pick number instead of name

--- src/core/domain/dtos/room.dto.ts
+++ src/core/domain/dtos/room.dto.ts
@@ -1,14 +1,14 @@
-import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
 
 import { RoomEntity } from '@/src/shared/infra/typeorm/entities/room.entity';
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
 export interface RoomDto extends RoomEntity {}
 
-export class CreateRoomDto implements Pick<RoomDto, 'name' | 'description' | 'subject'> {
-  @IsString()
+export class CreateRoomDto implements Pick<RoomDto, 'number' | 'description' | 'subject'> {
+  @IsNumber()
   @IsNotEmpty()
-  name: string;
+  number: number;
   
   @IsString()
   @IsNotEmpty()
