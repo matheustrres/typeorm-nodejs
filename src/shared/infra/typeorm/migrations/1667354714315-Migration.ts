c0c74f13a68f3e3743a0a89a917f6d738ed708fc
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-04 15:49:45 -0300

    style: indent code

--- src/shared/infra/typeorm/migrations/1667354714315-Migration.ts
+++ src/shared/infra/typeorm/migrations/1667354714315-Migration.ts
@@ -1,18 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1667354714315 implements MigrationInterface {
-    name = 'Migration1667354714315'
+  public name: string = 'Migration1667354714315'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "rooms" RENAME COLUMN "name" TO "number"`);
-        await queryRunner.query(`ALTER TABLE "rooms" DROP COLUMN "number"`);
-        await queryRunner.query(`ALTER TABLE "rooms" ADD "number" integer NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "rooms" RENAME COLUMN "name" TO "number"`);
+    await queryRunner.query(`ALTER TABLE "rooms" DROP COLUMN "number"`);
+    await queryRunner.query(`ALTER TABLE "rooms" ADD "number" integer NOT NULL`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "rooms" DROP COLUMN "number"`);
-        await queryRunner.query(`ALTER TABLE "rooms" ADD "number" character varying NOT NULL`);
-        await queryRunner.query(`ALTER TABLE "rooms" RENAME COLUMN "number" TO "name"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`ALTER TABLE "rooms" DROP COLUMN "number"`);
+    await queryRunner.query(`ALTER TABLE "rooms" ADD "number" character varying NOT NULL`);
+    await queryRunner.query(`ALTER TABLE "rooms" RENAME COLUMN "number" TO "name"`);
+  }
 }
