fed35a1460e1c26a7155b24b07485f8cfbc0e706
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-04 22:49:16 -0300

    fix: pick properties properly

--- src/core/domain/dtos/room.dto.ts
+++ src/core/domain/dtos/room.dto.ts
@@ -1,19 +1,23 @@
-import { IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional } from 'class-validator';
 
 import { RoomEntity } from '@/src/shared/infra/typeorm/entities/room.entity';
+import { SpecificationEntity } from '@/src/shared/infra/typeorm/entities/specification.entity';
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
 export interface RoomDto extends RoomEntity {}
 
-export class CreateRoomDto implements Pick<RoomDto, 'number' | 'description' | 'subject'> {
+export class CreateRoomDto implements Pick<RoomDto, 'number' | 'capacity' | 'specifications' | 'subject'> {
   @IsNumber()
   @IsNotEmpty()
   number: number;
   
-  @IsString()
-  @IsNotEmpty()
-  description: string;
+  @IsNumber()
+  @IsOptional()
+  capacity?: number;
 
+  @IsOptional()
+  specifications?: SpecificationEntity[];
+  
   @IsOptional()
   subject?: SubjectEntity;
 }
