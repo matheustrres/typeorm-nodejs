b8024734036d913fbd63e613a46b10f53d1663e6
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-04 23:21:25 -0300

    feat: add optional description prop

--- src/shared/utils/errors/database.error.ts
+++ src/shared/utils/errors/database.error.ts
@@ -1,14 +1,21 @@
 export type ErrorType = 'DUPLICATED' | 'INVALID' | 'UNKNOWN';
 
+interface ErrorOptions {
+  description?: string;
+  type?: ErrorType;
+}
+
 export class DatabaseError extends Error {
   public readonly message: string;
+  public readonly description?: string;
   public readonly type: ErrorType; 
 
-  constructor(message: string, type?: ErrorType) {
+  constructor(message: string, options?: ErrorOptions) {
     super(message);
 
     this.message = message;
-    this.type = type;
+    this.description = options.description;
+    this.type = options.type;
   }
 }
 
