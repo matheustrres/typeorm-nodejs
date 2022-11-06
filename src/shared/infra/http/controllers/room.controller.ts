3808b799f39f3ee81813ee5ee98bc3311cd6c781
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-06 18:44:40 -0300

    feat: create getOne and list routes

--- src/shared/infra/http/controllers/room.controller.ts
+++ src/shared/infra/http/controllers/room.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import {
   Controller,
+  Get,
   Middleware,
   Post
 } from '@overnightjs/core';
@@ -16,6 +17,8 @@ import { CreateRoomDto } from '@/src/core/domain/dtos/room.dto';
 
 import { AccountMiddleware } from '@/src/shared/infra/http/middlewares/account.middleware';
 import { AuthMiddleware } from '@/src/shared/infra/http/middlewares/auth.middleware';
+
+import { paginator } from '@/src/shared/utils/functions/paginator';
 
 @Controller('rooms')
 export class RoomController extends BaseController {
@@ -34,6 +37,41 @@ export class RoomController extends BaseController {
       const room: RoomEntity = await this.service.create(body);
       
       return response.status(201).send(room);
+    } catch (error) {
+      return this.sendErrorResponse(response, error);
+    }
+  }
+  
+  @Get(':id')
+  @Middleware([
+    AuthMiddleware,
+    AccountMiddleware(ProfileAccountType.ADMIN)
+  ])
+  public async getOne(request: Request, response: Response): Promise<Response> {
+    try {
+      const roomId: string = request.params.id;
+      const room: RoomEntity = await this.service.findById(roomId);
+      
+      return response.status(200).send(room);
+    } catch (error) {
+      return this.sendErrorResponse(response, error);
+    }
+  }
+  
+  @Get('')
+  @Middleware([
+    AuthMiddleware,
+    AccountMiddleware(ProfileAccountType.ADMIN)
+  ])
+  public async list(request: Request, response: Response): Promise<Response> {
+    try {
+      const { take, skip } = paginator(request);
+      
+      console.log({ take, skip })
+      
+      const rooms = await this.service.list(take, skip);
+      
+      return response.status(200).send(rooms)
     } catch (error) {
       return this.sendErrorResponse(response, error);
     }
