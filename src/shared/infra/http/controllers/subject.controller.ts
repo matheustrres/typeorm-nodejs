6621e13eab0f0554416a7bce45aff7a8deff64e5
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-09 18:40:32 -0300

    fix: output typing

--- src/shared/infra/http/controllers/subject.controller.ts
+++ src/shared/infra/http/controllers/subject.controller.ts
@@ -9,9 +9,10 @@ import {
 
 import { BaseController } from './base.controller';
 
-import { ProfileAccountType } from '@/src/shared/infra/typeorm/entities/profile.entity';
-import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
+import { SubjectResponse } from '@/src/core/infra/presenters/subject.presenter';
 import { SubjectService } from '@/src/services/subject.service';
+
+import { ProfileAccountType } from '@/src/shared/infra/typeorm/entities/profile.entity';
 
 import { CreateSubjectDto } from '@/src/core/domain/dtos/subject.dto';
 
@@ -49,9 +50,9 @@ export class SubjectController extends BaseController {
   public async create(request: Request, response: Response): Promise<Response> {
     try {
       const body: CreateSubjectDto = request.body;
-      const subjectRecord: SubjectEntity = await this.service.create(body);
+      const subject: SubjectResponse = await this.service.create(body);
 
-      return response.status(201).send(subjectRecord);
+      return response.status(201).send(subject);
     } catch (error) {
       return this.sendErrorResponse(response, error);
     }
@@ -64,12 +65,12 @@ export class SubjectController extends BaseController {
    * @param {Response} response - The response to the request
    * @returns {Promise<Response>}
    */
-  @Get(':id')
+  @Get(':subjectId')
   @Middleware(AuthMiddleware)
   public async getOne(request: Request, response: Response): Promise<Response> {
     try {
-      const subjectId: string = request.params.id;
-      const subject: SubjectEntity = await this.service.findById(subjectId);
+      const subjectId: string = request.params.subjectId;
+      const subject: SubjectResponse = await this.service.findById(subjectId);
       
       return response.status(200).send(subject);
     } catch (error) {
@@ -90,9 +91,9 @@ export class SubjectController extends BaseController {
     try {
       const { skip, take } = paginator(request);
       
-      const rooms = await this.service.list(skip, take);
+      const subjects: SubjectResponse[] = await this.service.list(skip, take);
       
-      return response.status(200).send(rooms);
+      return response.status(200).send(subjects);
     } catch (error) {
       return this.sendErrorResponse(response, error);
     }
@@ -113,7 +114,7 @@ export class SubjectController extends BaseController {
       const subjectId: string = request.body.subjectId;
       const roomId: string = request.body.roomId;
       
-      const subjectWithRoom: SubjectEntity = await this.service.setSubjectRoom(subjectId, roomId);
+      const subjectWithRoom: SubjectResponse = await this.service.setSubjectRoom(subjectId, roomId);
       
       return response.status(201).send(subjectWithRoom);
     } catch (error) {
