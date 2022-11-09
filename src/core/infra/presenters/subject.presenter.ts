0c4ae7009a47122c3fd1ed9c027dc6ed92fb3c18
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-09 18:30:44 -0300

    feat(infra/presenters): create SubjectPresenter

---
+++ src/core/infra/presenters/subject.presenter.ts
@@ -0,0 +1,39 @@
+import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
+
+export interface SubjectResponse extends SubjectEntity {}
+
+export class SubjectPresenter {
+  public static handleSingleInstance(subject: SubjectEntity): SubjectResponse {
+    return {
+      ...{
+        id: subject.id,
+        name: subject.name,
+        taughtBy: subject.taughtBy,
+        createdAt: subject.createdAt,
+        updatedAt: subject.updatedAt
+      },
+      ...(
+        subject.room && {
+          room: subject.room
+        }
+      ),
+      ...(
+        subject.enrolledStudents?.length && {
+          enrolledStudents: subject.enrolledStudents
+        }
+      )
+    }
+  }
+  
+  public static handleMultipleInstances(subjects: SubjectEntity[]): SubjectResponse[] {
+    const handledSubjects: SubjectResponse[] = [];
+    
+    for (const subject of subjects) {
+      handledSubjects.push(
+        SubjectPresenter.handleSingleInstance(subject)
+      );
+    }
+    
+    return handledSubjects;
+  }
+}
