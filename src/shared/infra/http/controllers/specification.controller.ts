ed435b6a004040dcb59cb21b5d31f9a0035fd8ad
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-10 14:45:21 -0300

    chore: add specifications listing route

--- src/shared/infra/http/controllers/specification.controller.ts
+++ src/shared/infra/http/controllers/specification.controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import {
   Controller,
+  Get,
   Middleware,
   Post
 } from '@overnightjs/core';
@@ -16,6 +17,8 @@ import { CreateSpecificationDto } from '@/src/core/domain/dtos/specification.dto
 
 import { AuthMiddleware } from '@/src/shared/infra/http/middlewares/auth.middleware';
 import { AccountMiddleware } from '@/src/shared/infra/http/middlewares/account.middleware';
+
+import { paginator } from '@/src/shared/utils/functions/paginator';
 
 @Controller('specifications')
 export class SpecificationController extends BaseController {
@@ -34,6 +37,23 @@ export class SpecificationController extends BaseController {
       const specification: SpecificationEntity = await this.service.create(body);
       
       return response.status(201).send(specification);
+    } catch (error) {
+      return this.sendErrorResponse(response, error);
+    }
+  }
+  
+  @Get('')
+  @Middleware([
+    AuthMiddleware,
+    AccountMiddleware(ProfileAccountType.ADMIN)
+  ])
+  public async listSpecifications(request: Request, response: Response): Promise<Response> {
+    try {
+      const { skip, take } = paginator(request);
+      
+      const specifications: SpecificationEntity[] = await this.service.list(skip, take);
+      
+      return response.status(200).send(specifications);
     } catch (error) {
       return this.sendErrorResponse(response, error);
     }
