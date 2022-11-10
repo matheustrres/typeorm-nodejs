84289fa4a16a73363b493aaf8cd241497444ccea
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-10 14:44:57 -0300

    chore: create findById and list methods

--- src/services/specification.service.ts
+++ src/services/specification.service.ts
@@ -1,8 +1,11 @@
+import { CreateSpecificationDto } from '@/src/core/domain/dtos/specification.dto';
+
+import { SpecificationEntity } from '@/src/shared/infra/typeorm/entities/specification.entity';
+
+import { DatabaseValidationError } from '@/src/shared/utils/errors/database.error';
+
 import { SpecificationRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
 import { ORMSpecificationRepository } from '@/src/core/infra/repositories/implementations/specification.repository';
-import { CreateSpecificationDto } from '@/src/core/domain/dtos/specification.dto';
-import { SpecificationEntity } from '@/src/shared/infra/typeorm/entities/specification.entity';
-import { DatabaseValidationError } from '@/src/shared/utils/errors/database.error';
 
 export class SpecificationService {
   constructor(private repository: SpecificationRepository = new ORMSpecificationRepository()) {}
@@ -21,5 +24,37 @@ export class SpecificationService {
     }
     
     return this.repository.create(data);
+  }
+  
+  public async findById(id: string): Promise<SpecificationEntity> {
+    const specification: SpecificationEntity = await this.repository.findById(id);
+    
+    if (!specification) {
+      throw new DatabaseValidationError(
+        'Unsuccessful specification search',
+        {
+          description: 'No specification were found the given ID',
+          type: 'INVALID'
+        }
+      );
+    }
+    
+    return specification;
+  }
+  
+  public async list(skip: number = 0, take: number = 10): Promise<SpecificationEntity[]> {
+    const specifications: SpecificationEntity[] = await this.repository.list(skip, take);
+    
+    if (!specifications.length) {
+      throw new DatabaseValidationError(
+        'Unsuccessful specifications listing',
+        {
+          description: 'No specification records were found',
+          type: 'INVALID'
+        }
+      );
+    }
+    
+    return specifications;
   }
 }
