8b51def04a8818eab20a29793531cc2a2546167f
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-10 14:20:57 -0300

    feat: create SpecificationRepository interface

--- src/core/domain/repositories/typeorm/interfaces/index.ts
+++ src/core/domain/repositories/typeorm/interfaces/index.ts
@@ -2,6 +2,7 @@ import { BaseRepository } from '@/src/core/domain/repositories/base.repository';
 
 import { ProfileEntity } from '@/src/shared/infra/typeorm/entities/profile.entity';
 import { RoomEntity } from '@/src/shared/infra/typeorm/entities/room.entity';
+import { SpecificationEntity } from '@/src/shared/infra/typeorm/entities/specification.entity';
 import { StudentEntity } from '@/src/shared/infra/typeorm/entities/student.entity';
 import { SubjectEntity } from '@/src/shared/infra/typeorm/entities/subject.entity';
 
@@ -15,11 +16,16 @@ export interface RoomRepository extends BaseRepository<RoomEntity> {
   findByNumber(number: number): Promise<RoomEntity|undefined>;
 }
 
+export interface SpecificationRepository extends BaseRepository<SpecificationEntity> {
+  findById(id: string): Promise<SpecificationEntity|undefined>;
+  findByName(name: string): Promise<SpecificationEntity|undefined>;
+}
+
 export interface StudentRepository extends BaseRepository<StudentEntity> {
   findById(id: string): Promise<StudentEntity|undefined>;
 }
 
 export interface SubjectRepository extends BaseRepository<SubjectEntity> {
-  findByName(name: string): Promise<SubjectEntity|undefined>;
   findById(id: string): Promise<SubjectEntity|undefined>;
+  findByName(name: string): Promise<SubjectEntity|undefined>;
 }
