ebf770eb7e019ae52e27960ecd558f78ccf36961
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-17 17:58:43 -0300

    docs: add docs with jsdoc

--- src/shared/utils/cache.ts
+++ src/shared/utils/cache.ts
@@ -7,37 +7,53 @@ const redisConfig: RedisConfigProps = config.get<
   RedisConfigProps
 >('app.resources.redis');
 
+/**
+ * Represents the main CacheManager 
+ */
 export class CacheManager {
-  private client: RedisClient;
-  private connected: boolean = false;
-
   private ONE_DAY_IN_MS: number = 86_400_000;
 
-  constructor() {
-    if (!this.connected) {
-      this.client = new IORedis({
-        ...redisConfig
-      });
+  constructor(private client: RedisClient = new IORedis(redisConfig)) {}
 
-      this.connected = true;
-    }
-  }
-
+  /**
+   * Deletes a key
+   * 
+   * @param {RedisKey} redisKey - The key to be deleted
+   * @returns {Promise<void>} 
+   */
   public async delete(redisKey: RedisKey): Promise<void> {
     await this.client.del(redisKey);
   }
 
+  /**
+   * Gets the parsed value of a key
+   * 
+   * @param {RedisKey} redisKey - The key to be found
+   * @returns {Promise<T|null>}
+   */
   public async get<T>(redisKey: RedisKey): Promise<T|null> {
     const data: string = await this.client.get(redisKey);
 
     return data ? JSON.parse(data) as T : null;
   }
 
+  /**
+   * Sets the value of a key
+   * 
+   * @param {RedisKey} redisKey - The key be filled
+   * @param {T} value - The value for the key
+   * @returns {Promise<string>}
+   */
   public async set<T>(redisKey: RedisKey, value: T): Promise<string> {
     return this.client.set(redisKey, JSON.stringify(value), 'PX', this.ONE_DAY_IN_MS);
   }
 
-  public async flushAllCache(): Promise<string> {
-    return this.client.flushall();
+  /**
+   * Removes all cached keys 
+   * 
+   * @returns {Promise<void>}; 
+   */
+  public async flushAllCache(): Promise<void> {
+    await this.client.flushall();
   }
 }
