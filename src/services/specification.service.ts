dc727e32073aba43a210a29f8410a6b19e9410a1
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-17 17:16:59 -0300

    chore: extend BaseService & implement abstract method

--- src/services/specification.service.ts
+++ src/services/specification.service.ts
@@ -1,3 +1,5 @@
+import { BaseService } from './base.service';
+
 import { CreateSpecificationDto } from '@/src/core/domain/dtos/specification.dto';
 
 import { SpecificationEntity } from '@/src/shared/infra/typeorm/entities/specification.entity';
@@ -7,60 +9,57 @@ import { DatabaseValidationError } from '@/src/shared/utils/errors/database.erro
 import { SpecificationRepository } from '@/src/core/domain/repositories/typeorm/interfaces';
 import { ORMSpecificationRepository } from '@/src/core/infra/repositories/implementations/specification.repository';
 
-export class SpecificationService {
-  constructor(private repository: SpecificationRepository = new ORMSpecificationRepository()) {}
-  
+export class SpecificationService extends BaseService {
+  constructor(private repository: SpecificationRepository = new ORMSpecificationRepository()) {
+    super();
+  }
+
   public async create(data: CreateSpecificationDto): Promise<SpecificationEntity> {
     const specificationAlreadyExists: SpecificationEntity = await this.repository.findByName(data.name);
-    
+
     if (specificationAlreadyExists) {
-      throw new DatabaseValidationError(
-        'Unsuccessful specification creation',
-        {
-          description: 'A specification already exists with the given name',
-          type: 'DUPLICATED'
-        }
-      );
+      throw new DatabaseValidationError('Unsuccessful specification creation', {
+        description: 'A specification already exists with the given name',
+        type: 'DUPLICATED'
+      });
     }
-    
+
     return this.repository.create(data);
   }
-  
+
   public async delete(id: string): Promise<void> {
     const specification: SpecificationEntity = await this.findById(id);
-    
+
     await this.repository.delete(specification.id);
   }
-  
+
   public async findById(id: string): Promise<SpecificationEntity> {
     const specification: SpecificationEntity = await this.repository.findById(id);
-    
+
     if (!specification) {
-      throw new DatabaseValidationError(
-        'Unsuccessful specification search',
-        {
-          description: 'No specification were found the given ID',
-          type: 'INVALID'
-        }
-      );
+      throw new DatabaseValidationError('Unsuccessful specification search', {
+        description: 'No specification were found the given ID',
+        type: 'INVALID'
+      });
     }
-    
+
     return specification;
   }
-  
+
   public async list(skip: number = 0, take: number = 10): Promise<SpecificationEntity[]> {
     const specifications: SpecificationEntity[] = await this.repository.list(skip, take);
-    
+
     if (!specifications.length) {
-      throw new DatabaseValidationError(
-        'Unsuccessful specifications listing',
-        {
-          description: 'No specification records were found',
-          type: 'INVALID'
-        }
-      );
+      throw new DatabaseValidationError('Unsuccessful specifications listing', {
+        description: 'No specification records were found',
+        type: 'INVALID'
+      });
     }
-    
+
     return specifications;
+  }
+
+  public getCacheKey(id: string): string {
+    return `--specification-${id}`;
   }
 }
