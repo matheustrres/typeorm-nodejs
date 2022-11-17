09b484a7b35102df4ec2ba71326b9533e730e266
Matheus Torres <torresmiranda.rj@gmail.com>
2022-11-17 16:08:54 -0300

    feat: implement caching

--- src/services/profile.service.ts
+++ src/services/profile.service.ts
@@ -27,7 +27,7 @@ export class ProfileService extends BaseService {
   ) {
     super();
   }
-  
+
   /**
    * Authenticates a profile and assigns it a JWT
    *
@@ -37,36 +37,30 @@ export class ProfileService extends BaseService {
    */
   public async authenticate(email: string, password: string): Promise<string> {
     const profile: ProfileEntity = await this.repository.findByEmail(email);
-    
+
     if (!profile) {
-      throw new DatabaseValidationError(
-        'Authentication failed',
-        {
-          description: 'Invalid credentials were given',
-          type: 'INVALID'
-        }
-      );
+      throw new DatabaseValidationError('Authentication failed', {
+        description: 'Invalid credentials were given',
+        type: 'INVALID'
+      });
     }
-    
+
     const validPassword: boolean = await AuthProvider.comparePasswords(password, profile.password);
-    
+
     if (!validPassword) {
-      throw new DatabaseValidationError(
-        'Authentication failed',
-        {
-          description: 'Invalid credentials were given',
-          type: 'INVALID'
-        }
-      );
+      throw new DatabaseValidationError('Authentication failed', {
+        description: 'Invalid credentials were given',
+        type: 'INVALID'
+      });
     }
-    
+
     return AuthProvider.signToken({
       id: profile.id,
       accountType: profile.accountType,
       studentId: profile.studentProfile?.id
     });
   }
-  
+
   /**
    * Creates a profile
    *
@@ -80,31 +74,28 @@ export class ProfileService extends BaseService {
    */
   public async create(data: CreateProfileDto): Promise<ProfileResponse> {
     const profileAlreadyExists: ProfileEntity = await this.repository.findByEmail(data.email);
-    
+
     if (profileAlreadyExists) {
-      throw new DatabaseValidationError(
-        'Unsuccessful profile creation',
-        {
-          description: 'A profile already exists with the given email',
-          type: 'DUPLICATED'
-        }
-      );
+      throw new DatabaseValidationError('Unsuccessful profile creation', {
+        description: 'A profile already exists with the given email',
+        type: 'DUPLICATED'
+      });
     }
-    
-    const profile = await this.repository.create({
+
+    const profile: ProfileEntity = await this.repository.create({
       ...data,
       password: await AuthProvider.hashPassword(data.password)
     });
-    
+
     if (profile.accountType === 'student') {
       profile.studentProfile = await this.studentService.create({ profile });
-      
+
       await this.repository.update(profile);
     }
-  
+
     return ProfilePresenter.handleSingleInstance(profile);
   }
-  
+
   /**
    * Deletes a profile
    *
@@ -112,11 +103,19 @@ export class ProfileService extends BaseService {
    * @returns {Promise<void>}
    */
   public async delete(id: string): Promise<void> {
+    const cachedProfile: ProfileEntity = await this.cacheManager.get<
+      ProfileEntity
+    >(this.getCacheKey(id));
+
+    if (cachedProfile) {
+      await this.cacheManager.delete(this.getCacheKey(id));
+    }
+
     const profile: ProfileResponse = await this.findById(id);
-    
+
     await this.repository.delete(profile.id);
   }
-  
+
   /**
    * Finds a profile by its id
    *
@@ -124,21 +123,30 @@ export class ProfileService extends BaseService {
    * @returns {Promise<ProfileResponse>}
    */
   public async findById(id: string): Promise<ProfileResponse> {
-    const profile: ProfileEntity = await this.repository.findById(id);
-    
-    if (!profile) {
-      throw new DatabaseValidationError(
-        'Unsuccessful profile search',
-        {
-          description: 'No profile were found with the given ID',
-          type: 'INVALID'
-        }
-      );
+    const cachedProfile: ProfileEntity = await this.cacheManager.get<
+      ProfileEntity
+    >(this.getCacheKey(id));
+
+    if (cachedProfile) {
+      return ProfilePresenter.handleSingleInstance(cachedProfile);
     }
-    
+
+    const profile: ProfileEntity = await this.repository.findById(id);
+
+    if (!profile) {
+      throw new DatabaseValidationError('Unsuccessful profile search', {
+        description: 'No profile were found with the given ID',
+        type: 'INVALID'
+      });
+    }
+
+    await this.cacheManager.set<
+      ProfileEntity
+    >(this.getCacheKey(id), profile);
+
     return ProfilePresenter.handleSingleInstance(profile);
   }
-  
+
   /**
    * Lists all profile records
    *
@@ -148,16 +156,14 @@ export class ProfileService extends BaseService {
    */
   public async list(skip: number = 0, take: number = 10): Promise<ProfileResponse[]> {
     const profiles: ProfileEntity[] = await this.repository.list(skip, take);
-    
+
     if (!profiles.length) {
-      throw new DatabaseValidationError(
-        'Unsuccessful profiles listing',
-        {
-          description: 'No profile records were found'
-        }
-      );
+      throw new DatabaseValidationError('Unsuccessful profiles listing', {
+        description: 'No profile records were found',
+        type: 'INVALID'
+      });
     }
-  
+
     return ProfilePresenter.handleMultipleInstances(profiles);
   }
 
